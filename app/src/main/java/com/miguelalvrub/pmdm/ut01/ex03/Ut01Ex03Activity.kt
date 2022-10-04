eddee2b46a299f8e69ac7a61a9040cc8cd37ef06
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2022-10-04 17:08:41 +0200

    Start Project

---
+++ app/src/main/java/com/miguelalvrub/pmdm/ut01/ex03/Ut01Ex03Activity.kt
@@ -0,0 +1,12 @@
+package com.miguelalvrub.pmdm.ut01.ex03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miguelalvrub.pmdm.R
+
+class Ut01Ex03Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex03)
+    }
+}
