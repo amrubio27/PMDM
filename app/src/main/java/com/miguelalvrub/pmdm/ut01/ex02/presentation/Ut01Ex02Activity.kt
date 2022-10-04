6af872ac0115a87c6eb942b910c7e0d7a7b5c0e7
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2022-10-04 16:21:20 +0200

    Start Project

---
+++ app/src/main/java/com/miguelalvrub/pmdm/ut01/ex02/presentation/Ut01Ex02Activity.kt
@@ -0,0 +1,12 @@
+package com.miguelalvrub.pmdm.ut01.ex02.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miguelalvrub.pmdm.R
+
+class Ut01Ex02Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ejerciciopropuesto)
+    }
+}
