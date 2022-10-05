0f90665ecfb8b53e3b1be9b14aaa1f41f8d221cd
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2022-10-05 16:52:48 +0200

    Create Branch for Ut01Ex04 (Create a copy in style of an existing app using Relative Layout, AlignParent, toRightOf, etc)

---
+++ app/src/main/java/com/miguelalvrub/pmdm/ut01/ex04/Ut01Ex04Activity.kt
@@ -0,0 +1,12 @@
+package com.miguelalvrub.pmdm.ut01.ex04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miguelalvrub.pmdm.R
+
+class Ut01Ex04Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex04)
+    }
+}
